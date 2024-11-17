a7fd82c25fb79747e85f63e021e12737f16b45cb
AdalOnShow <mdadalbadsha2008@gmail.com>
2024-11-17 19:27:54 +0600

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Nav from './Nav'
+import Hero from './Hero'
+import PoplarProducts from './PoplarProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services'
+import SpecialOffer from './SpecialOffer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+  Nav,
+  Hero,
+  PoplarProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+}
