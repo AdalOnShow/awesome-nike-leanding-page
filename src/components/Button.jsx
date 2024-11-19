6ce96e0441ce99e719a01e2d50269af998b8cd2b
AdalOnShow <mdadalbadsha2008@gmail.com>
2024-11-19 19:08:21 +0600

    ready PoplarProducts, SuperQuality, Services and SpecialOffer sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border border-coral-red">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none rounded-full
+      ${backgroundColor ? `${backgroundColor} ${textColor} border ${borderColor}` : `bg-coral-red text-white border border-coral-red`}`}>
       {label}
 
-      <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full size-5" />
+      {iconURL && <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full size-5" />}
     </button>
   )
 }
--- src/sections/PoplarProducts.jsx
+++ src/sections/PoplarProducts.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import PoplarProductCard from "../components/PoplarProductCard"
+import { products } from "../constants"
 
 const PoplarProducts = () => {
   return (
-    <div>PoplarProducts</div>
+    <section id="products" className="max-container max-sm:mt-12 ">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red"> Poplar </span>Products</h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design, and value</p>
+      </div>
+
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product, i) => (
+          <PoplarProductCard key={i} {...product} />
+        ))}
+      </div>
+    </section>
   )
 }
 
