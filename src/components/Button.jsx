4fbfbd43d8850f29f8508c763280af2ca5962e59
AdalOnShow <mdadalbadsha2008@gmail.com>
2024-11-19 20:20:09 +0600

    ready CustomerReviews, Subscribe and Footer sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none rounded-full
-      ${backgroundColor ? `${backgroundColor} ${textColor} border ${borderColor}` : `bg-coral-red text-white border border-coral-red`}`}>
+      ${backgroundColor ? `${backgroundColor} ${textColor} border ${borderColor}` : `bg-coral-red text-white border border-coral-red`} ${fullWidth && 'w-full'}`}>
       {label}
 
       {iconURL && <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full size-5" />}
--- src/constants/index.js
+++ src/constants/index.js
@@ -118,7 +118,7 @@ export const footerLinks = [
 ];
 
 export const socialMedia = [
-  { src: facebook, alt: "facebook logo" },
-  { src: twitter, alt: "twitter logo" },
-  { src: instagram, alt: "instagram logo" },
+  { src: facebook, alt: "facebook logo", href: "http://www.facebook.com" },
+  { src: twitter, alt: "twitter logo", href: "http://www.twitter.com" },
+  { src: instagram, alt: "instagram logo", href: "http://www.instagram.com" },
 ];
